434f895d7dddabf30f255808913c41e13345fc0e
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2022-12-16 21:43:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import streamlit as st
-
+import tensorflow as tf 
+import tensorflow_hub as hub
+import numpy as np
+import pd 
 
 # https://en.wikipedia.org/wiki/Andor_(TV_series)
 
+def load_images():
+
+    return content, style
 
 # Create the main app
 def main():
     st.title("Neural Style Transfer")
 
-    
+    content, style = load_images()
 
 
 if __name__ == "__main__":
