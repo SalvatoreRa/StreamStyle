0f38abb6da4def482f6c53d54b9b881f0d4c145a
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2022-12-16 21:52:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ import pd
 # https://en.wikipedia.org/wiki/Andor_(TV_series)
 
 def load_images():
-
+     content = st.file_uploader("Choose the image to paint!")
+     style = st.file_uploader("Choose the style!")
     return content, style
+
+def load_model():
+    model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+    return model
 
 # Create the main app
 def main():
