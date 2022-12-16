e7fa588841ebe4e133b754178cd84151bc5971c4
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2022-12-16 22:05:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,27 @@ def load_images():
      style = st.file_uploader("Choose the style!")
     return content, style
 
+def process_input(img):
+  max_dim = 1024
+  img = tf.keras.preprocessing.image.img_to_array(img)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+  new_shape = tf.cast(shape * scale, tf.int32)
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  img /= 255.0
+  return img
+
+def process_output(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
+
 def load_model():
     model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
     return model
